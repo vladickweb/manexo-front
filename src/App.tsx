fa222f972410cb14872231a330a3f860d9b9aec6
Vladyslav Kapkan Panchenko <vladyslavkap@ext.inditex.com>
2025-03-14 16:20:11 +0100

    feat(package.json): add Tailwind CSS and its Vite plugin as dependencies to enable utility-first CSS styling style(App.tsx): apply Tailwind CSS class to paragraph for improved styling style(index.css): integrate Tailwind CSS and clean up CSS formatting for consistency fix(vite.config.ts): include Tailwind CSS plugin in Vite configuration for proper integration

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='bg-red-500'>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
