2ac5b380a4203ac3c02fe5e1fd165e30c4892200
Vladyslav Kapkan Panchenko <vladyslavkap@ext.inditex.com>
2025-03-16 11:10:23 +0100

    feat(routes): implement routing with TanStack Router and create basic routes for home and about pages chore(dependencies): add @tanstack/react-router and related devtools for improved routing capabilities refactor(styles): remove unused App.css and related assets to streamline the project structure refactor(App): replace App component with RouterProvider to utilize new routing setup fix(vite.config): update Vite configuration to include TanStack Router plugin for routing support

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import { createRoot } from "react-dom/client";
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import { QueryClient } from "@tanstack/react-query";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 
-import App from "./App.tsx";
+import { routeTree } from "./routeTree.gen";
 
 import "./index.css";
 
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -23,7 +31,7 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
 );
