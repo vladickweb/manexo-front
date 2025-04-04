67c12e094e7f3acc5cbdf14ca28f65e6984bc0fe
Vladyslav Kapkan Panchenko <vladyslavkap@ext.inditex.com>
2025-04-05 00:20:42 +0200

    fix: fix build

--- src/modules/FavoritesPage/index.tsx
+++ src/modules/FavoritesPage/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { FaHeart } from "react-icons/fa";
 
 import { MainLayout } from "@/layouts/MainLayout";
@@ -7,7 +5,6 @@ import { MainLayout } from "@/layouts/MainLayout";
 export const FavoritesPage = () => {
   return (
     <MainLayout>
-      {/* Main Content */}
       <div className="flex flex-col items-center justify-center h-[60vh]">
         <div className="w-20 h-20 rounded-full bg-primary/10 flex items-center justify-center mb-4">
           <FaHeart className="w-10 h-10 text-primary" />
